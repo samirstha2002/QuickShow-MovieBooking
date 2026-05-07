3449b69f3de1a5e236c67a8d8bfec90ab2d289ff
samirstha2002 <154325590+samirstha2002@users.noreply.github.com>
2026-05-07 16:55:03 +0545

    movie detail page description

--- client/src/components/MovieCard.jsx
+++ client/src/components/MovieCard.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import timeFormat from "../lib/timeFormat";
 
 function MovieCard({ movie }) {
-  const navigate = useNavigate;
+  const navigate = useNavigate();
   return (
     <div className="flex flex-col justify-between p-3 bg-gray-800 rounded-2xl hover:-translate-y-1 transition duration-300 w-66">
       <img
