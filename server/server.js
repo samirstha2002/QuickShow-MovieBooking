47ef1cba2a18ce5ee1cf3ac63c5179b984431c45
samirstha2002 <154325590+samirstha2002@users.noreply.github.com>
2026-05-11 16:06:55 +0545

    databse connection and also inngest use and vercel

--- server/server.js
+++ server/server.js
@@ -1,16 +1,23 @@
 import express from "express";
-import dotenv from "dotenv";
+import "dotenv/config";
 import cors from "cors";
+import connectDB from "./config/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import { serve } from "inngest/express";
+import { inngest, functions } from "./ingest/index.js";
 
 const app = express();
 const port = 3000;
 
+await connectDB();
 //Middleware
 app.use(express.json());
 app.use(cors());
+app.use(clerkMiddleware());
 
 //Api Routes
 app.get("/", (req, res) => res.send("Server is Live"));
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.listen(port, () =>
   console.log(`Server listening at http://localhost:${port}`),
