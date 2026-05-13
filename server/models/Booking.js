4939d2a8d833691d6995f1e148ba3e5ab15e681e
samirstha2002 <154325590+samirstha2002@users.noreply.github.com>
2026-05-13 15:00:07 +0545

    admin dashbaord api

--- server/models/Booking.js
+++ server/models/Booking.js
@@ -1,1 +1,17 @@
 import mongoose from "mongoose";
+
+const bookingSchema = new mongoose.Schema(
+  {
+    user: { type: String, required: true, ref: "User" },
+    show: { type: String, required: true, ref: "Show" },
+    amount: { type: Number, required: true },
+    bookedSeats: { type: Array, required: true },
+    isPaid: { type: Boolean, default: false },
+    paymentLink: { type: String },
+  },
+  { timeStamps: true },
+);
+
+const Booking = mongoose.model("Booking", bookingSchema);
+
+export default Booking;
